84a31259d13aa13af7ae1c1aaf1c96e3a739bffd
Gerardo Miranda <gerardodev25@gmail.com>
2024-01-09 18:16:46 -0400

    chore(Store): :construction: Mostrar los productos
    Mostrar los productos
    
    Mostrar los productos

--- src/products/api/productsApi.ts
+++ src/products/api/productsApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const productApi = axios.create({
-    baseURL: '  http://localhost:3100/products',
+    baseURL: '  http://localhost:3100',
 });
 
 export { productApi };
