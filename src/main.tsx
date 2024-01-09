ef8dd257641c807f5b264cd524079b7dff7889e9
Gerardo Miranda <gerardodev25@gmail.com>
2024-01-09 17:24:45 -0400

    build(Store): :tada: Inicio de proyecto
    Inicio de proyecto
    
    Inicio de proyecto

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import { NextUIProvider } from "@nextui-org/react";
+import { NextUIProvider } from '@nextui-org/react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router.tsx';
 
@@ -10,9 +10,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <NextUIProvider>
-      <main className="dark text-foreground bg-background">
-        <RouterProvider router={ router } />
+      <main className='dark text-foreground bg-background'>
+        <RouterProvider router={router} />
       </main>
     </NextUIProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
