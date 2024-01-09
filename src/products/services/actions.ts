34ff10d2623fcbb2d520a68e397c3c21c1e4bc93
Gerardo Miranda <gerardodev25@gmail.com>
2024-01-09 18:32:57 -0400

    chore(Store): :construction: Artículos de Hombre y Mujer
    Artículos de Hombre y Mujer
    
    Artículos de Hombre y Mujer

--- src/products/services/actions.ts
+++ src/products/services/actions.ts
@@ -4,11 +4,21 @@ interface GetProductOptions {
   filterKey?: string;
 }
 
+const sleep = async (seconds: number): Promise<boolean> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      return resolve(true);
+    }, seconds * 1000);
+  });
+};
+
 export const getProducts = async ({
   filterKey,
 }: GetProductOptions): Promise<Product[]> => {
-  const { data } = await productApi.get<Product[]>(`/products`);
+  await sleep(2);
+  const filteredURL = filterKey ? `category=${filterKey}` : '';
+
+  const { data } = await productApi.get<Product[]>(`/products?${filteredURL}`);
 
   return data;
 };
-
