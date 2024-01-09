dc08648e690e35946709f5f2504081d3aa466a27
Gerardo Miranda <gerardodev25@gmail.com>
2024-01-09 17:42:49 -0400

    refactor(Store): :construction: Configurar TanStack Query
    Configurar TanStack Query
    
    Configurar TanStack Query

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,22 @@ import ReactDOM from 'react-dom/client';
 
 import { NextUIProvider } from '@nextui-org/react';
 import { RouterProvider } from 'react-router-dom';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import { TanStackProvider } from './plugins/TanStackProvider.tsx';
 import { router } from './router/router.tsx';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <main className='dark text-foreground bg-background'>
-        <RouterProvider router={router} />
-      </main>
-    </NextUIProvider>
+    <TanStackProvider>
+      <NextUIProvider>
+        <main className='dark text-foreground bg-background'>
+          <RouterProvider router={router} />
+        </main>
+        <ReactQueryDevtools initialIsOpen={false}/>
+      </NextUIProvider>
+    </TanStackProvider>
   </React.StrictMode>
 );
