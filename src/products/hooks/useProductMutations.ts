02c2b35fa49458b4e98f0502f0177431385e4dea
Gerardo Miranda <gerardodev25@gmail.com>
2024-01-10 18:29:51 -0400

    chore(Store): :construction: Evitar invalidar el query
    v
    
    Evitar invalidar el query

--- src/products/hooks/useProductMutations.ts
+++ src/products/hooks/useProductMutations.ts
@@ -1,16 +1,24 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { productActions } from '..';
+import { Product, productActions } from '..';
 
 export const useProductMutations = () => {
   const queryClient = useQueryClient();
 
   const mutation = useMutation({
     mutationFn: productActions.createProduct,
-    onSuccess: (data) => {
-      console.log('product created');
-      queryClient.invalidateQueries({
-        queryKey: ['products', { filterKey: data.category }],
-      });
+    onSuccess: (product) => {
+      // queryClient.invalidateQueries({
+      //   queryKey: ['products', { filterKey: data.category }],
+      // });
+
+      queryClient.setQueriesData<Product[]>(
+        ['products', { filterKey: product.category }],
+        (old) => {
+          if (!old) {
+            return [old];
+          } else return [...old, product];
+        }
+      );
     },
   });
 
